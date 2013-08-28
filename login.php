e04e4f7d385f6adda08da8bfa02abad2ba033b1b
andres Ramirez <autorun@live.co.uk>
2013-08-28 16:19:44 -0500

    login page added

---
+++ login.php
@@ -0,0 +1,17 @@
+
+<!DOCTYRPE html>
+<html class="no-js" lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta charset=utf-8>
+        <title>title</title>
+ 
+        <!--[if lt IE 9]>
+            <script src="//html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+        <![endif]-->
+ 
+    </head>
+    <body>
+        Login
+    </body>
+</html>
